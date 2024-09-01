e8699acf83b0df97f91a8dfbd60592524dde1f8b
alimur16 <murtadhoali22@gmail.com>
2024-09-01 11:05:09 +0700

    category filament

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
+ */
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+
+    protected $model = Category::class;
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'slug' => fake()->slug(),
+            'description' => fake()->text(100),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+    }
+}
